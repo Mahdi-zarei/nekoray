1feda430902d7f890b0746593346e1cda51df4cc
Nova <Mahdi.zrei@gmail.com>
2024-11-07 22:59:43 +0330

    init proxy types

--- include/global.h
+++ include/global.h
@@ -1,8 +1,105 @@
-//
-// Created by Mahdi on 11/7/2024.
-//
+#pragma once
+#include <QList>
+#include <QString>
 
-#ifndef GLOBALS_H
-#define GLOBALS_H
+namespace Constants
+{
+    namespace ProxyTypes
+    {
+        inline const QString Socks = "socks";
+        inline const QString HTTP = "http";
+        inline const QString ShadowSocks = "shadowsocks";
+        inline const QString Trojan = "trojan";
+        inline const QString VMess = "vmess";
+        inline const QString VLESS = "vless";
+        inline const QString ShadowTLS = "shadowtls";
+        inline const QString Hysteria = "hysteria";
+        inline const QString Hysterya2 = "hysteria2";
+        inline const QString Tuic = "tuic";
+        inline const QString WireGuard = "wireguard";
+        inline const QString SSH = "ssh";
+        inline const QString Chain = "chain";
 
-#endif //GLOBALS_H
+        inline const QStringList proxyTypes = {
+            Socks, HTTP, ShadowSocks, Trojan, VMess, VLESS, ShadowTLS, Hysteria, Hysterya2, Tuic, WireGuard, SSH, Chain
+        };
+
+        inline bool validate(QString const &str)
+        {
+            return proxyTypes.contains(str);
+        }
+    }
+
+    namespace MuxEnableModes
+    {
+        inline int MuxDefault = 0;
+        inline int MuxOn = 1;
+        inline int MuxOff = 2;
+    }
+
+    namespace MuxTypes
+    {
+        inline const QString Smux = "smux";
+        inline const QString Yamux = "yamux";
+        inline const QString H2mux = "h2mux";
+
+        inline const QStringList muxTypes = {Smux, Yamux, H2mux};
+
+        inline bool validate(QString const& str)
+        {
+            return muxTypes.contains(str);
+        }
+    }
+
+    namespace TransportTypes
+    {
+        inline const QString HTTP = "http";
+        inline const QString WebSocket = "ws";
+        inline const QString QUIC = "quic";
+        inline const QString GRPC = "grpc";
+        inline const QString HTTPUpgrade = "httpupgrade";
+
+        inline const QStringList transportTypes = {HTTP, WebSocket, QUIC, GRPC, HTTPUpgrade};
+
+        inline bool validate(QString const& str)
+        {
+            return transportTypes.contains(str);
+        }
+    }
+
+    namespace TLSTypes
+    {
+        inline const QString TLS = "tls";
+        inline const QString Reality = "reality";
+
+        inline const QStringList tlsTypes = {TLS, Reality};
+
+        inline bool validate(QString const& str)
+        {
+            return tlsTypes.contains(str);
+        }
+    }
+
+    namespace TLSFingerPrintTypes
+    {
+        inline const QString Chrome = "chrome";
+        inline const QString Firefox = "firefox";
+        inline const QString Edge = "edge";
+        inline const QString Safari = "safari";
+        inline const QString Browser360 = "360";
+        inline const QString Qq = "qq";
+        inline const QString Ios = "ios";
+        inline const QString Android = "android";
+        inline const QString Random = "random";
+        inline const QString Randomized = "randomized";
+
+        inline const QStringList tlsFingerPrintTypes = {
+            Chrome, Firefox, Edge, Safari, Browser360, Qq, Ios, Android, Random, Randomized
+        };
+
+        inline bool validate(QString const& str)
+        {
+            return tlsFingerPrintTypes.contains(str);
+        }
+    }
+}
